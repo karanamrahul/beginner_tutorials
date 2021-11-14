f5aa9c81777391481892af509cf3983dcc5a008d
Rahul karanam <rahulkarnam777@gmail.com>
2021-11-14 16:37:36 -0500

    Updated main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,6 @@ int main(int argc,
          char **argv)
 {
   ros::init(argc, argv, "test_pub");
-  nh.reset(new ros::NodeHandle);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
